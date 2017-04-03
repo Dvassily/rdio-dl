1d36c35799f3bc6fc27d60651e27b6eec16b4cd9
Basil Dalié <Basil Dali>
2017-04-03 08:50:21 +0200

    Add artist and song name to title

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-var fileName = document.getElementsByClassName('song__artist')[0].innerHTML + '-' + document.getElementsByClassName('song__title')[0].innerHTML;
-document.getElementsByClassName('social_links_icons')[0].innerHTML = '<br /><a type="application/mpeg" href="' + request.url  + '" download="true">Download</a>';
+var filename = document.getElementsByClassName('song__artist')[0].innerHTML + '-' + document.getElementsByClassName('song__title')[0].innerHTML + '.mp3';
+
+if (document.getElementById('dl-button') === null) {
+    document.getElementsByClassName('social_links_icons')[0].innerHTML += '<button id="dl-button">Download</button>';
+}
+
+document.getElementById('dl-button').addEventListener("click", function() {
+    browser.runtime.sendMessage({
+	fileName : filename
+    });
+});
+
